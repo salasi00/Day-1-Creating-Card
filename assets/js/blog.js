b5ab72cf9731bbf6f28f7cf3da3aaa592b0bdd27
rizal salasi <bogeymanyolo@gmail.com>
2022-12-26 06:29:17 +0700

    day 5 counting blog time

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,5 +1,4 @@
 let myProject = []
-let checkboxes  =  []
 
 function getData(event){
     event.preventDefault()
@@ -7,24 +6,29 @@ function getData(event){
     let startDate = document.getElementById("input-start-date").value
     let endDate = document.getElementById("input-end-date").value
     let blogContent = document.getElementById("input-blog-contents").value
-    let techEl = document.getElementsByName("technologies")
-    let image = document.getElementById("input-blog-image")
+    let nodeJsEl = document.getElementById("nodejs")
+    let reactJsEl = document.getElementById("reactjs")
+    let nextJsEl = document.getElementById("nextjs")
+    let typeScriptEl = document.getElementById("typescript")
+    let image = document.getElementById("input-blog-image").files
 
-for (let i = 0; i <techEl.length; i++){
-    if (techEl[i].checked){
-        checkboxes.push(techEl[i].value)
-    }
-}
+    let nodeJs = (nodeJsEl.checked === true) ? "<i class='fab fa-node-js'></i>" : ""
+    let reactJs = (reactJsEl.checked === true) ? "<i class='fab fa-react'></i>" : ""
+    let nextJs = (nextJsEl.checked === true) ? "assets/images/nextjs.png" : ""
+    let typeScript = (typeScriptEl.checked === true) ? "assets/images/typescr.png" : ""
 
 
+console.log(image)
 
-    console.log(image)
+
+    
 
     if (projectName == "" || startDate == "" || endDate == "" || blogContent == ""
         || image == "") {
             return alert("All input must not be empty!")
         }
-    image = URL.createObjectURL(image.files[0]);
+    image = URL.createObjectURL(image[0]);
+    console.log(image)
 
     let project = {
         projectName,
@@ -32,18 +36,25 @@ for (let i = 0; i <techEl.length; i++){
         endDate,
         blogContent,
         image,
-        checkboxes,
+        nodeJs,
+        reactJs,
+        nextJs,
+        typeScript,
         postedAt : new Date(),
     };
     myProject.push(project);
     renderBlog()
+
+
+
+    console.table(myProject)
 }
 
 function renderBlog() {
     document.getElementById("contents").innerHTML= "";
 
     for (let i = 0; i < myProject.length; i++){
-        document.getElementById("contents").innerHTML +=
+        document.getElementById("contents").innerHTML += `
         <div class="list-container">
                     <div class="project-list-item">
                         <div class="project-list-thumbnail">
@@ -51,21 +62,16 @@ function renderBlog() {
                         </div>
                         <div class="project-list-title">
                             <h4>${myProject[i].projectName}</h4>
-                            <p>durasi: 3 Bulan</p>
+                            <p>Durasi: ${getTimeDifference(myProject[i].startDate, myProject[i].endDate)}</p>
                         </div>
                         <div class="project-list-content">
                             <p>${myProject[i].blogContent}</p>
                         </div>
-                        <div>
-                            <span>
-                                
-                            </span>
-                            <span>
-                                
-                            </span>
-                            <span>
-                                
-                            </span>
+                        <div class="project-list-icon">
+                                ${myProject[i].nodeJs}         
+                                ${myProject[i].reactJs} 
+                                <img src="${myProject[i].nextJs}" alt="">
+                                <img src="${myProject[i].typeScript}" alt="">
                         </div>
                         <div class="list-input-container">
                             <div>
@@ -77,5 +83,25 @@ function renderBlog() {
                         </div>
                     </div>
                 </div>
+        `
     }
 }
+
+function getTimeDifference(startDate, endDate){
+    let start = new Date(startDate)
+    let end = new Date(endDate)
+
+    let ms = 1000
+
+    let timeDifference = end - start
+
+    let differenceMonth = Math.floor(timeDifference / (ms * 3600 * 24 * 30))
+    let differenceDay = Math.floor(timeDifference / (ms * 3600 * 24))
+
+    if (differenceMonth > 0){
+        return `${differenceMonth} Month Ago`
+    } else {
+        return `${differenceDay} Days Ago`
+    }
+
+} 

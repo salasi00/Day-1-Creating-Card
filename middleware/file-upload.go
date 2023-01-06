ed3c03ece1a8e6c0bbfedb76237607bc4edc8470
rizal salasi <bogeymanyolo@gmail.com>
2023-01-06 22:45:01 +0700

    day 12 - relation table & file upload

---
+++ middleware/file-upload.go
@@ -0,0 +1,43 @@
+package middleware
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func UploadFile(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		file, handler, err := r.FormFile("input-blog-images")
+		if err != nil {
+			fmt.Println(err)
+			json.NewEncoder(w).Encode("Error retrieving the file")
+			return
+		}
+		defer file.Close()
+		fmt.Printf("Uploaded file: %+v\n", handler.Filename)
+
+		tempFile, err := ioutil.TempFile("uploads", "image-*"+handler.Filename)
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("Path upload error")
+			json.NewEncoder(w).Encode(err)
+			return
+		}
+		defer tempFile.Close()
+
+		fileBytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			fmt.Println(err)
+		}
+		tempFile.Write(fileBytes)
+
+		data := tempFile.Name()
+		filename := data[8:] // slice 8 char dari nama filenya
+		
+		ctx := context.WithValue(r.Context(), "dataFile", filename)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
